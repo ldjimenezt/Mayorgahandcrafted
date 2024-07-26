1958a6fc47ddc5c00e0e5040a4530a282fe1fdcf
ldjimenezt <ldjimenezt@utn.edu.ec>
2024-07-25 21:28:44 -0500

    home-page-complete

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -6,7 +6,7 @@ const ProductList = () => {
     <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap">
       <Link
         href="/test"
-        className="w-full flex flex-col gap-4 sm:w-[45%] lg:[22%]"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
       >
         <div className="relative w-full h-80">
           <Image
@@ -24,7 +24,107 @@ const ProductList = () => {
             className="absolute object-cover rounded-md"
           />
         </div>
+        <div className="flex justify-between">
+          <span className="font-medium">Backpack</span>
+          <span className="font-semibold">$250</span>
+        </div>
+        <div className="text-sm text-gray-500">
+          Discover our Backpack Collection: durable, stylish, and designed for
+          everyday use. Perfect for work, school, or travel.
+        </div>
+        <button className="rounded-2xl ring-1 ring-lama text-lama w-max py-2 px-4 text-xs hover:bg-lama hover:text-white">Add to Cart</button>
       </Link>
+      <Link
+        href="/test"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-full h-80">
+          <Image
+            src="/cart-1.png"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src="/hero-4.png"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium">Backpack</span>
+          <span className="font-semibold">$250</span>
+        </div>
+        <div className="text-sm text-gray-500">
+          Discover our Backpack Collection: durable, stylish, and designed for
+          everyday use. Perfect for work, school, or travel.
+        </div>
+        <button className="rounded-2xl ring-1 ring-lama text-lama w-max py-2 px-4 text-xs hover:bg-lama hover:text-white">Add to Cart</button>
+      </Link>
+      <Link
+        href="/test"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-full h-80">
+          <Image
+            src="/cart-1.png"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src="/hero-4.png"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium">Backpack</span>
+          <span className="font-semibold">$250</span>
+        </div>
+        <div className="text-sm text-gray-500">
+          Discover our Backpack Collection: durable, stylish, and designed for
+          everyday use. Perfect for work, school, or travel.
+        </div>
+        <button className="rounded-2xl ring-1 ring-lama text-lama w-max py-2 px-4 text-xs hover:bg-lama hover:text-white">Add to Cart</button>
+      </Link>
+      <Link
+        href="/test"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-full h-80">
+          <Image
+            src="/cart-1.png"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src="/hero-4.png"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium">Backpack</span>
+          <span className="font-semibold">$250</span>
+        </div>
+        <div className="text-sm text-gray-500">
+          Discover our Backpack Collection: durable, stylish, and designed for
+          everyday use. Perfect for work, school, or travel.
+        </div>
+        <button className="rounded-2xl ring-1 ring-lama text-lama w-max py-2 px-4 text-xs hover:bg-lama hover:text-white">Add to Cart</button>
+      </Link>
+      
     </div>
   );
 };
