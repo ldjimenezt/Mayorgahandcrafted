230a25cadcc549ddd1a004f75944426679206d16
ldjimenezt <ldjimenezt@utn.edu.ec>
2024-07-28 01:17:40 -0500

    add-features

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,37 +1,48 @@
 import Filter from "@/components/Filter";
 import ProductList from "@/components/ProductList";
+import { wixClientServer } from "@/lib/wixClientServer";
 import Image from "next/image";
+import { Suspense } from "react";
 
-const ListPage = () => {
-    return (
-        <div className="px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 relative">
-            {/* CAMPAIGN */}
-            <div className="hidden bg-gradient-to-r from-blue-50 to-yellow-50 sm:flex justify-between h-64">
-                <div className="w-2/3 flex flex-col items-center justify-center gap-8">
-                    <h1 className="text-4xl font-semibold leading-[48px] text-gray-700">
-                        Grab up to 20% off on
-                        <br /> Selected Products
-                    </h1>
-                    <button className="rounded-md bg-black border border-black text-white hover:bg-transparent hover:text-black w-max py-3 px-5 text-sm">
-                        Buy Now
-                    </button>
-                </div>
-                <div className="relative w-1/3">
-                    <Image 
-                        src="/hero-4.png" 
-                        alt="Hero Image" 
-                        fill
-                        className="object-contain rounded-lg" 
-                    />
-                </div>
-            </div>
-            {/* FILTER */}
-            <Filter />
-            {/* PRODUCTS */}
-            <h1 className="mt-12 text-xl font-semibold">Shoes For You!</h1>
-            <ProductList />
+const ListPage = async ({ searchParams }: { searchParams: any }) => {
+  const wixClient = await wixClientServer();
+
+  const cat = await wixClient.collections.getCollectionBySlug(
+    searchParams.cat || "all-products"
+  );
+
+  
+  return (
+    <div className="px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 relative">
+      {/* CAMPAIGN */}
+      <div className="hidden bg-gradient-to-r from-blue-50 to-yellow-50 sm:flex justify-between h-64">
+        <div className="w-2/3 flex flex-col items-center justify-center gap-8">
+          <h1 className="text-4xl font-semibold leading-[48px] text-gray-700">
+            Grab up to 20% off on
+            <br /> Selected Products
+          </h1>
+          <button className="rounded-md bg-black border border-black text-white hover:bg-transparent hover:text-black w-max py-3 px-5 text-sm">
+            Buy Now
+          </button>
         </div>
-    );
-}
+        <div className="relative w-1/3">
+          <Image
+            src="/hero-4.png"
+            alt="Hero Image"
+            fill
+            className="object-contain rounded-lg"
+          />
+        </div>
+      </div>
+      {/* FILTER */}
+      <Filter />
+      {/* PRODUCTS */}
+      <h1 className="mt-12 text-xl font-semibold">Shoes For You!</h1>
+      <Suspense fallback={"loading..."}>
+        <ProductList categoryId={cat.collection?._id || "00000000-000000-000000-000000000001"} searchParams={searchParams} />
+      </Suspense>
+    </div>
+  );
+};
 
 export default ListPage;
